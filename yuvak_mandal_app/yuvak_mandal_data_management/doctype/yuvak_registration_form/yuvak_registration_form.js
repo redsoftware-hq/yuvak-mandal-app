c9dea78ed40f5ea2f305cdb69722e49af6db08cb
AksharPrajapati03 <akshar.prajapati@redsoftware.in>
2024-02-27 12:23:24 +0530

    added doctypes

---
+++ yuvak_mandal_app/yuvak_mandal_data_management/doctype/yuvak_registration_form/yuvak_registration_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Akshar prajapati and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Yuvak Registration Form", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ yuvak_mandal_app/yuvak_mandal_data_management/doctype/yuvak_registration_form/yuvak_registration_form.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Akshar prajapati and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class YuvakRegistrationForm(Document):
+	pass
